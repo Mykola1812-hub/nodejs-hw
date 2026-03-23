16f89531107f7ddf8d9a800cbaa41e2f4a7f52ce
Mykola <koliazelman@gmail.com>
2026-03-23 21:46:33 +0100

    re-do

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -27,7 +27,7 @@ export const deleteNote = async (req, res) => {
   const note = await Note.findOneAndDelete({_id: noteId,});
 
   if (!note) {
-    throw createHttpError(404, 'Note not Found!');
+    throw createHttpError(404, 'Note not found!');
   }
 
   res.status(200).json(note);
@@ -38,7 +38,7 @@ export const updateNote = async (req, res) => {
   const note = await Note.findOneAndUpdate({ _id: noteId }, req.body, {returnDocument: 'after',});
 
   if (!note) {
-    throw createHttpError(404, 'Note not Found!');
+    throw createHttpError(404, 'Note not found!');
   }
 
   res.status(200).json(note);
