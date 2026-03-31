70cc7d906de809258d6c5f11f5db658d1f42ccfd
Mykola <koliazelman@gmail.com>
2026-04-01 01:21:42 +0200

    do all tasks

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import { createNote, deleteNote, getAllNotes, getNoteById, updateNote } from '../controllers/notesController.js';
 import { celebrate } from 'celebrate';
-import { createNoteSchema, getAllNotesSchema, noteIdSchema } from '../validations/notesValidation.js';
+import { createNoteSchema, getAllNotesSchema, noteIdSchema, updateNoteSchema } from '../validations/notesValidation.js';
 
 const router = Router();
 
@@ -9,7 +9,7 @@ router.get('/notes', celebrate(getAllNotesSchema), getAllNotes);
 router.get('/notes/:noteId', celebrate(noteIdSchema) ,getNoteById);
 router.post('/notes', celebrate(createNoteSchema),createNote);
 router.delete('/notes/:noteId', celebrate(noteIdSchema) ,deleteNote);
-router.patch('/notes/:noteId', updateNote);
+router.patch('/notes/:noteId', celebrate(updateNoteSchema),updateNote);
 
 
 export default router;
